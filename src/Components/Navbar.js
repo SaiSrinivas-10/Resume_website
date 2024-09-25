cc5cef581f5bef12375c959d2d20acded6374373
Sai Srinivas <71049633+SaiSrinivas-10@users.noreply.github.com>
2024-09-24 20:48:16 -0400

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './styles/navbar.css'
+import {FaGithubSquare,FaLinkedin,FaPhoneSquare, FaHackerrank} from 'react-icons/fa'
+
 // import {Container, Navbar, Nav,NavDropdown} from 'react-bootstrap';
 
 export const NavBar = () => {
