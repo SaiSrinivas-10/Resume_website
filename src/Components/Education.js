48bf90e0d61b9bd7902811bab8971bfa61f3e400
Sai Srinivas <71049633+SaiSrinivas-10@users.noreply.github.com>
2024-09-24 20:28:39 -0400

    Education.js changes in Masters university updates.

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -8,12 +8,25 @@ export const Education = () => {
             <div className="intro-text">
                 <h1>100 EDUCATION</h1>
                 <div className="paragraphs">
+
+                    <h2 style = {{color : "orange", textAlign:"left", paddingTop:"2rem"}}>Masters in Computer Science</h2>
+                    <h4 style = {{color : "white", textAlign:"left", paddingTop:"1rem"}}>George Mason University</h4>
+                    <h4 style = {{color : "white", textAlign:"left", paddingTop:"1rem"}}>Computer Science</h4>
+                    <h4 style = {{color : "orange", textAlign:"left", paddingTop:"2rem" , paddingBottom:"1rem"}}>Academic Coursework : </h4>
+                <p>
+                    <li><strong>Artificial Intelligence and Machine Learning:</strong> Gained hands on experience by building projects using search algorithms and rebuilt the pacman game using the reinforcement learning.</li>        
+                    // <li><strong>Data Analysis and Visualization:</strong> Learned about data cleaning, manipulation, and visualization using tools such as R and Python.</li>      
+                    // <li><strong>Computer Programming and Data Structures:</strong> Covered programming concepts in C, C++, Java and data structures such as arrays, linked lists, stacks, and queues.</li>
+                    // <li> <strong>Design and Analysis of Algorithms:</strong> Learned about various algorithmic techniques and their applications in problem-solving.</li>
+                    // <li> <strong>Web Technologies:</strong> Gained proficiency in web development using HTML, CSS, JavaScript, and PHP.</li>
+                </p>
+
                     <h2 style = {{color : "orange", textAlign:"left", paddingTop:"2rem"}}>Bachelor of Technology</h2>
                     <h4 style = {{color : "white", textAlign:"left", paddingTop:"1rem"}}>Jawaharlal Nehru Technological University</h4>
                     <h4 style = {{color : "white", textAlign:"left", paddingTop:"1rem"}}>Computer Science and Information Technology</h4>
                     <h4 style = {{color : "orange", textAlign:"left", paddingTop:"2rem" , paddingBottom:"1rem"}}>Academic Coursework : </h4>
                 <p>
-                    <li><strong>Artificial Intelligence and Machine Learning:</strong> Acquired knowledge in AI and ML concepts such as supervised and unsupervised learning, decision trees, and neural networks, statistics, etc.  </li>        
+                    <li><strong>Artificial Intelligence and Machine Learning:</strong> Acquired theoritical knowledge in AI and ML concepts such as supervised and unsupervised learning, decision trees, and neural networks, statistics, etc.  </li>        
                     <li><strong>Data Analysis and Visualization:</strong> Learned about data cleaning, manipulation, and visualization using tools such as R and Python.</li>      
                     <li><strong>Computer Programming and Data Structures:</strong> Covered programming concepts in C, C++, Java and data structures such as arrays, linked lists, stacks, and queues.</li>
                     <li> <strong>Design and Analysis of Algorithms:</strong> Learned about various algorithmic techniques and their applications in problem-solving.</li>
